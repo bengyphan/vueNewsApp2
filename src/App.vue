928c19550c1e01f07f2df49713b0ca8db7c755b6
bengyphan <bengyphan@gmail.com>
2019-09-28 15:42:17 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app" class="container-fluid">
+    <div class="row">
+      <app-search v-on:newsChanged="getNews"></app-search>
+    </div>
+    <div>
+      <label for="title">Title</label>
+      <input type="radio" id="title" value="title"
+        v-model="sortCriteria">
+      <br>
+      <label for="author">Author</label>
+      <input type="radio" id="author" value="author"
+        v-model="sortCriteria">
+    </div>
+    <div class="row">
+      <app-article
+        v-for="newsArticle in sortedArticles"
+        v-bind:data="newsArticle">
+      </app-article>
+    </div>
+  </div>
+</template>
+
+<script>
+import Article from './components/Article.vue'
+import Search from './components/Search.vue'
+
+export default {
+  data: function() {
+    return {
+      articles: [],
+      searchQ: 'politics',
+      sortCriteria: ''
+    }
+  },
+  methods: {
+    getNews: function(query) {
+      var that = this;
+      var url = 'https://newsapi.org/v2/everything?' +
+          'q=' + 
+          query + 
+          '&' +
+          'apiKey=333b406605bc432485a9569e2241abf4';
+      var req = new Request(url);
+      fetch(req)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data);
+          that.articles = data.articles;
+        })
+        
+        this.searchQ = '';
+        this.sortCriteria = "";
+    },
+    sortBy: function(arr, sortCrit) {
+      return arr.sort(function(a,b) {
+        if (a[sortCrit] > b[sortCrit]) return 1;
+        if (a[sortCrit] < b[sortCrit]) return -1;
+        return 0;
+      })
+    }
+  },
+  computed: {
+    sortedArticles: function() {
+      if(this.sortCriteria) {
+        return this.sortBy(this.articles, this.sortCriteria);
+      }
+      return this.articles;
+    }
+  },
+  components: {
+    'app-article': Article,
+    'app-search': Search
+  },
+  mounted: function(){
+    this.getNews(this.searchQ);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Article.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-4">
+        <img 
+        class="img-fluid"
+        v-bind:src="data.urlToImage" 
+        v-bind:alt="data.title">
+        <h4 >{{ limitChar(data.title) }}</h4>
+        <h6>{{ data.author }} | {{ data.source.name }}</h6>
+        <p>{{ limitChar(data.description) }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['data'],
+        methods: {
+            limitChar: function(string) {
+                let body = this.stripTags(string);
+                return body.length > 100 ? body.substring(0, 100) + '...' : body;
+            },
+            stripTags: function(text) {
+                return text.replace(/(<([^>]+)>)/ig, '');
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
